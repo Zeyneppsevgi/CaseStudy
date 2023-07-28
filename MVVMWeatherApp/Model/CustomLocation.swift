eb1d1c82ff9ad59bc524b9252d41040fde00f008
Zeyneppsevgi <zeyneppsevgi@gmail.com>
2023-07-28 15:22:38 +0300

    Weather app with MVVM

---
+++ MVVMWeatherApp/Model/CustomLocation.swift
@@ -0,0 +1,13 @@
+//
+//  Location.swift
+//  MVVMWeatherApp
+//
+//  Created by Zeynep Sevgi on 28.07.2023.
+//
+
+import Foundation
+
+struct CustomLocation : Codable {
+    let lat: Double
+    let lon: Double
+}
